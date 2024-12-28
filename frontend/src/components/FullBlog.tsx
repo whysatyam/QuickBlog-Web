256a5f410cfbd7d0c68a3133b60a0cb82afbcd25
satyam <satyamremote@gmail.com>
2024-12-28 21:04:52 +0530

    commit

--- frontend/src/components/FullBlog.tsx
+++ frontend/src/components/FullBlog.tsx
@@ -8,19 +8,19 @@ export const FullBlog = ({ blog }: { blog: Blog }) => {
       <Appbar />
       <div className="flex justify-center">
         <div className="grid grid-cols-12 h-screen w-full px-10 pt-12 max-w-screen-xl">
-          <div className="col-span-8">
+          <div className="col-span-12 md:col-span-8">
             <div className="text-2xl font-bold">{blog.title}</div>
-            <div className="py-5 text-gray-500">All Rights Reserved</div>
+            <div className="py-5 font-serif text-gray-500">Create | Share | Inspire</div>
             <div className="pt-3 h-screen">{blog.content}</div>
           </div>
-          <div className="col-span-4">
+          <div className="col-span-12 md:col-span-4 hidden md:block">
             <div className="flex justify-center gap-2">
               <div className="flex flex-col justify-center">
                 <Avatar size="big" name={blog.author.name || "Anonymous"} />
               </div>
               <div>
-                <div className="text-md pt-2 text-gray-500">Author</div>
-                <div className=" text-1xl font-bold">
+                <div className="text-md text-gray-500">Author</div>
+                <div className="text-1xl font-bold">
                   {blog.author.name || "Anonymous"}
                 </div>
               </div>
